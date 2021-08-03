895213de4ba3f41ad2835f9b3bd784b44b305ffc
AzureAD\GeorgeMarian <george.marian@principal33.com>
2021-08-03 12:56:59 +0300

    style cop analyzer

--- HelloWorldWebApp/Controllers/HomeController.cs
+++ HelloWorldWebApp/Controllers/HomeController.cs
@@ -1,11 +1,8 @@
 ﻿using HelloWorldWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace HelloWorldWebApp.Controllers
 {
