2fab4df594812db32945b00ea9bd601bb1cc693f
AzureAD\GeorgeMarian <george.marian@principal33.com>
2021-08-03 10:38:08 +0300

    Add item to the list

---
+++ HelloWorldWebApp/wwwroot/js/Index.js
@@ -0,0 +1,12 @@
+﻿// This JS file now uses jQuery. Pls see here: https://jquery.com/
+$(document).ready(function () {
+    // see https://api.jquery.com/click/
+    $("#addMembersButton").click(function () {
+        var newcomerName = $("#nameField").val();
+
+        // Remember string interpolation
+        $("#teamMembers").append(`<li>${newcomerName}</li>`);
+
+        $("#nameField").val("");
+    })
+});
