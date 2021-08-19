7078cb69f76ad16b325a572ecbea21fe97683a49
AzureAD\GeorgeMarian <george.marian@principal33.com>
2021-08-19 10:57:20 +0300

    function pointers

---
+++ CSharpFeatures/Coffee.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharpFeatures
+{
+    internal class Coffee
+    {
+        public Coffee()
+        {
+        }
+        public Coffee(string name)
+        {
+            CoffeeType = name;
+        }
+
+        public string CoffeeType { get; set; }
+
+        public override string ToString()
+        {
+            return CoffeeType;
+        }
+    }
+}
--- CSharpFeatures/Program.cs
+++ CSharpFeatures/Program.cs
@@ -11,21 +11,56 @@ namespace CSharpFeatures
     {
         static void Main(string[] args)
         {
-            ITimeService timeService = new TimeService();
-            TeamMember teamMember = new TeamMember("member1", timeService);
+            //ITimeService timeService = new TimeService();
+            //TeamMember teamMember = new TeamMember("member1", timeService);
 
-            string json = JsonSerializer.Serialize(teamMember);
-            Console.WriteLine(json);
+            //string json = JsonSerializer.Serialize(teamMember);
+            //Console.WriteLine(json);
 
-            File.WriteAllText("TeamMember.json",json);
+            //File.WriteAllText("TeamMember.json",json);
 
-            Task<string> jsonTask = File.ReadAllTextAsync("TeamMember.json");
-            jsonTask.Wait();
-            string jsonFile = jsonTask.Result;
+            //Task<string> jsonTask = File.ReadAllTextAsync("TeamMember.json");
+            //jsonTask.Wait();
+            //string jsonFile = jsonTask.Result;
 
-            TeamMember deserializerMember = JsonSerializer.Deserialize<TeamMember>(jsonFile);
+            //TeamMember deserializerMember = JsonSerializer.Deserialize<TeamMember>(jsonFile);
 
-            Console.WriteLine(deserializerMember);
+            Console.Write("What would you like?");
+            string input = Console.ReadLine();
+            Func<string, string, string, string, Coffee> receipe = input == "FlatWhite" ? FlatWhite : Espresso;
+
+            Coffee coffe = MakeCoffe("Grains", "Milk", "Water", "Sugar", receipe);
+            Console.WriteLine($"Here is your coffee: {coffe}.");
+
+        }
+
+        static public Coffee MakeCoffe(string grains, string milk, string water, string sugar, Func<string, string, string, string, Coffee> recipe)
+        {
+            try
+            {
+                Console.WriteLine("Start preparing the coffe");
+                var coffe = recipe(grains, milk, water, sugar);
+                return coffe;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("Finished");
+            }
+        }
+
+        static public Coffee Espresso(string grains, string milk, string water, string sugar)
+        {
+            return new Coffee("Espresso");
+        }
+        
+        static public Coffee FlatWhite(string grains, string milk, string water, string sugar)
+        {
+            return new Coffee("FlatWhite");
         }
     }
 }
