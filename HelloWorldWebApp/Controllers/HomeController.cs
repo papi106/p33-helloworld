0b7008ce09d3477ab702f4c61c7e2f4bdc8d38de
AzureAD\GeorgeMarian <george.marian@principal33.com>
2021-08-20 10:36:00 +0300

    add that boilerplate

--- HelloWorldWebApp/Controllers/HomeController.cs
+++ HelloWorldWebApp/Controllers/HomeController.cs
@@ -63,5 +63,10 @@ namespace HelloWorldWebApp.Controllers
         {
             return this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
         }
+        
+        public IActionResult Chat()
+        {
+            return View();
+        }
     }
 }
